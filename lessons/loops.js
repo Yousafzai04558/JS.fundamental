9a183e03633f862d564347a49c5c64c6d1392b05
ahmad6304 <mushtaq6304@gmail.com>
2026-09-03 21:29:03 +0500

    js.fundamental

---
+++ lessons/loops.js
@@ -0,0 +1,18 @@
+// loops
+
+// for loop
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+var car = ["BMW", "Volvo", "Saab", "Ford"];
+for (let i = 0; i < car.length; i++) {
+  console.log(car[i]);
+  if (car[i] === "Volvo") {
+    break;
+  }
+}
+
+// ES6 syntax for each loop
+car.forEach (car => {
+  console.log(car);
+});
